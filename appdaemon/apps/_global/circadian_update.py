132cf3beb3ccde9b933076d7e084325683895e1e
andreypolyak <andrey.polyak@gmail.com>
2021-06-11 13:59:58 +0300

    Add morning coeff to circadian update

--- appdaemon/apps/_global/circadian_update.py
+++ appdaemon/apps/_global/circadian_update.py
@@ -1,10 +1,14 @@
 import appdaemon.plugins.hass.hassapi as hass
 import math
+import datetime
 
 DELAY = 120
-STEP = 2
-MAX = 300
-MIN = 10
+SAT_STEP = 2
+MAX_LUX = 300
+MIN_LUX = 10
+MORNING_START = 6
+MORNING_END = 12
+PCT = 0.2
 
 
 class CircadianUpdate(hass.Hass):
@@ -20,13 +24,33 @@ class CircadianUpdate(hass.Hass):
       lux = float(int(self.get_state("sensor.balcony_illuminance")))
     except ValueError:
       return None
-    if lux >= MAX:
+    if lux >= MAX_LUX:
       return 0
-    elif lux < MIN:
+    elif lux < MIN_LUX:
       return 100
     else:
-      saturation = int(math.ceil((MAX - lux) / ((MAX - MIN) / (100 / STEP - 1)))) * STEP
+      saturation = int(math.ceil((MAX_LUX - lux) / ((MAX_LUX - MIN_LUX) / (100 / SAT_STEP - 1)))) * SAT_STEP
+      saturation = saturation * self.calculate_morning_coeff()
       return saturation
+
+
+  def calculate_morning_coeff(self):
+    now = datetime.datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    seconds = (now - midnight).seconds
+    start_time = MORNING_START * 3600
+    end_time = MORNING_END * 3600
+    middle_time = (end_time - start_time) / 2 + start_time
+    for i in range(0, 23):
+      seconds = 3600 * i
+      if seconds < start_time or seconds > end_time:
+        morning_coeff = 1
+      elif seconds >= start_time and seconds <= middle_time:
+        morning_coeff = 1 - (1 - (middle_time - seconds) / (middle_time - start_time)) * PCT
+      elif seconds > middle_time and seconds <= end_time:
+        morning_coeff = 1 - (end_time - seconds) / (end_time - middle_time) * PCT
+    return morning_coeff
+
 
   def calculate_kelvin(self, saturation):
     kelvin = 2000 + saturation * ((6500 - 2000) / 100)
@@ -41,11 +65,11 @@ class CircadianUpdate(hass.Hass):
     kelvin = self.calculate_kelvin(new_saturation)
     old_saturation = self.get_old_saturation()
     if self.get_state("light.ha_group_all") == "on":
-      if abs(new_saturation - old_saturation) > STEP:
+      if abs(new_saturation - old_saturation) > SAT_STEP:
         if new_saturation > old_saturation:
-          new_saturation = old_saturation + STEP
+          new_saturation = old_saturation + SAT_STEP
         elif new_saturation < old_saturation:
-          new_saturation = old_saturation - STEP
+          new_saturation = old_saturation - SAT_STEP
     if new_saturation != old_saturation:
       self.set_saturation(new_saturation, kelvin)
 
