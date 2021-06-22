ab97421a0c95e9f887b15eaafcedad2b0405969c
andreypolyak <andrey.polyak@gmail.com>
2021-06-22 16:06:37 +0300

    Water cooler should be turned on when anyone has district location

--- appdaemon/apps/kitchen/cooler.py
+++ appdaemon/apps/kitchen/cooler.py
@@ -4,35 +4,27 @@ import appdaemon.plugins.hass.hassapi as hass
 class Cooler(hass.Hass):
 
   def initialize(self):
-    self.listen_state(self.on_scene_change, "input_select.living_scene")
-    self.listen_state(self.on_lights_off, "light.ha_group_kitchen", new="off")
-    binary_sensors = self.get_state("binary_sensor")
-    for binary_sensor in binary_sensors:
-      if binary_sensor.endswith("_motion") and "kitchen" in binary_sensor:
-        self.listen_state(self.on_motion, binary_sensor, new="on", old="off")
-    self.turn_on_cooler({})
+    self.persons = self.get_app("persons")
+    for entity in self.persons.get_all_person_location_entities():
+      self.listen_state(self.process, entity)
+    self.listen_state(self.process, "light.ha_group_kitchen")
+    self.listen_state(self.process, "input_select.living_scene")
+    self.turn_on_cooler()
 
 
-  def on_lights_off(self, entity, attribute, old, new, kwargs):
-    if self.get_state("input_select.living_scene") == "night":
-      self.turn_off_cooler({})
-
-
-  def on_scene_change(self, entity, attribute, old, new, kwargs):
-    if new in ["night", "away"]:
-      self.turn_off_cooler({})
+  def process(self, entity, attribute, old, new, kwargs):
+    people_inside_district = self.persons.is_any_person_inside_location("district")
+    lights_on = self.get_state("light.ha_group_kitchen") == "on"
+    living_scene = self.get_state("input_select.living_scene")
+    if (not people_inside_district and living_scene == "away") or (living_scene == "night" and not lights_on):
+      self.turn_off_cooler()
     else:
-      self.turn_on_cooler({})
+      self.turn_on_cooler()
 
 
-  def on_motion(self, entity, attribute, old, new, kwargs):
-    if self.get_state("input_select.living_scene") != "away":
-      self.turn_on_cooler({})
-
-
-  def turn_on_cooler(self, kwargs):
+  def turn_on_cooler(self):
     self.call_service("switch/turn_on", entity_id="switch.kitchen_cooler_plug")
 
 
-  def turn_off_cooler(self, kwargs):
+  def turn_off_cooler(self):
     self.call_service("switch/turn_off", entity_id="switch.kitchen_cooler_plug")
