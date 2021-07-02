3e41f9aca7006e3f5ad1d8528970d555b0caf92a
andreypolyak <andrey.polyak@gmail.com>
2021-07-02 17:19:26 +0300

    Add new ESP to track weight

--- appdaemon/apps/bedroom/weight.py
+++ appdaemon/apps/bedroom/weight.py
@@ -16,20 +16,10 @@ class Weight(hass.Hass):
     for person in self.persons.get_all_persons():
       person_name = person["name"]
       self.storage.init(f"weight.{person_name}", default)
-    self.listen_state(self.on_scales_change, "sensor.scales")
-    self.listen_state(self.on_scales_change, "sensor.scales_theo")
-    self.listen_state(self.on_weight_change, "input_number.weight")
+    self.listen_state(self.on_scales_change, "sensor.bedroom_scales")
 
 
   def on_scales_change(self, entity, attribute, old, new, kwargs):
-    try:
-      weight = float(new)
-    except ValueError:
-      return
-    self.call_service("input_number/set_value", entity_id="input_number.weight", value=weight)
-
-
-  def on_weight_change(self, entity, attribute, old, new, kwargs):
     if new in ["unavailable", "unknown", "None"] or old in ["unavailable", "unknown", "None"]:
       return
     try:
