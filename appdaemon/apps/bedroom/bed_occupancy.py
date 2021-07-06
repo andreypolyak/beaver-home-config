7bcab3994890ec08eae0ce786bd38baed9981c4d
andreypolyak <andrey.polyak@gmail.com>
2021-07-06 19:19:54 +0300

    Automatically turn on night scene in sleeping zone when Theo bed occupied

--- appdaemon/apps/bedroom/bed_occupancy.py
+++ appdaemon/apps/bedroom/bed_occupancy.py
@@ -9,6 +9,7 @@ class BedOccupancy(hass.Hass):
     for sensor in SENSORS:
       self.listen_state(self.on_change, f"binary_sensor.{sensor}", new="off", old="on")
     self.listen_state(self.on_change, "input_select.living_scene", new="day", old="night")
+    self.listen_state(self.on_theo_bed_occupancy, "binary_sensor.bedroom_theo_bed_occupancy", new="on", old="off")
 
 
   def on_change(self, entity, attribute, old, new, kwargs):
@@ -23,4 +24,8 @@ class BedOccupancy(hass.Hass):
     if not night_scene_in_living_zone_enough:
       return
     self.log(f"Turning on day scene in sleeping zone because {entity} state changed")
-    self.call_service("input_select/select_option", entity_id="input_select.sleeping_scene", option="day")
+
+  def on_theo_bed_occupancy(self, entity, attribute, old, new, kwargs):
+    if self.now_is_between("20:00:00", "10:00:00") and self.get_scene("sleeping") == "day":
+      self.log(f"Turning on night scene in sleeping zone because of occupancy in Theo bed")
+      self.set_scene("sleeping", "night")
