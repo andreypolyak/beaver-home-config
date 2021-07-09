3c1b98c17e3ebe0ff00f0b5c1474d281468d35f2
andreypolyak <andrey.polyak@gmail.com>
2021-07-09 18:32:36 +0300

    Refactor is media playing

--- appdaemon/apps/global/is_media_playing.py
+++ appdaemon/apps/global/is_media_playing.py
@@ -7,13 +7,10 @@ class IsMediaPlaying(Base):
     super().initialize()
     self.handles = {}
     self.sonos_devices = []
-    media_players = self.get_state("media_player")
-    for media_player in media_players.keys():
-      if "_sonos" in media_player:
-        device = media_player.replace("media_player.", "")
-        self.sonos_devices.append(device)
-        self.handles[device] = None
-        self.listen_state(self.on_sonos_change, media_player)
+    self.find_all_sonos_devices()
+    for sonos_device in self.sonos_devices:
+      self.handles[sonos_device] = None
+      self.listen_state(self.on_sonos_change, sonos_device)
     self.tv_devices = ["universal_apple_tv", "universal_playstation_4"]
     self.handles["living_room_tv"] = None
     for device in self.tv_devices:
@@ -22,12 +19,20 @@ class IsMediaPlaying(Base):
     self.run_every(self.check_state, "now", 600)
 
 
+  def find_all_sonos_devices(self):
+    self.sonos_devices = []
+    media_players = self.get_state("media_player")
+    for media_player in media_players.keys():
+      if "sonos" in media_player:
+        sonos_device = media_player.replace("media_player.", "")
+        self.sonos_devices.append(sonos_device)
+
+
   def on_tv_change(self, entity, attribute, old, new, kwargs):
     self.check_tv_state()
 
 
   def on_sonos_change(self, entity, attribute, old, new, kwargs):
-    self.log("Sonos change")
     self.check_sonos_state(entity.replace("media_player.", ""))
 
 
@@ -51,42 +56,45 @@ class IsMediaPlaying(Base):
 
 
   def check_tv_state(self):
-    self.log("Checking TV State")
     tv_source = self.get_state("input_select.current_universal_tv_source")
     if self.get_living_scene() == "party":
       return
-    is_tv_in_living_room_sonos = self.get_state("media_player.living_room_sonos", attribute="source") == "TV"
     if self.is_entity_on(f"media_player.universal_{tv_source}"):
-      self.log("TV On")
+      self.log(f"TV device {tv_source} is on")
       self.cancel_media_handle("living_room_tv")
       self.turn_on_entity("input_boolean.living_room_tv_playing")
-      if is_tv_in_living_room_sonos:
+      if self.is_tv_source():
         self.turn_off_entity("input_boolean.living_room_sonos_playing")
     elif self.is_entity_off(f"media_player.universal_{tv_source}"):
-      self.log("TV Off")
+      self.log(f"TV device {tv_source} is off")
       self.cancel_media_handle("living_room_tv")
       self.handles["living_room_tv"] = self.run_in(self.set_not_playing, 60, device="living_room_tv")
 
 
   def check_sonos_state(self, entity):
-    sonos_state = self.get_state(f"media_player.{entity}")
     if self.get_living_scene() == "party":
       return
+    sonos_state = self.get_state(f"media_player.{entity}")
     device = entity.replace("media_player.", "")
-    is_tv_in_living_room_sonos = self.get_state("media_player.living_room_sonos", attribute="source") == "TV"
     if (
       sonos_state == "playing"
       and (
-        (device == "living_room_sonos" and not is_tv_in_living_room_sonos)
+        (device == "living_room_sonos" and not self.is_tv_source())
         or device != "living_room_sonos"
       )
     ):
+      self.log(f"Device {device} is playing")
       self.cancel_media_handle(device)
       self.turn_on_entity(f"input_boolean.{device}_playing")
     elif sonos_state == "paused":
+      self.log(f"Device {device} is paused")
       self.cancel_media_handle(device)
       self.handles[device] = self.run_in(self.set_not_playing, 60, device=device)
 
 
   def cancel_media_handle(self, device):
     self.cancel_handle(self.handles[device])
+
+
+  def is_tv_source(self):
+    return self.get_state("media_player.living_room_sonos", attribute="source") == "TV"
