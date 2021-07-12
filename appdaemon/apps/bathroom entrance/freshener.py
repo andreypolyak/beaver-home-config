6d0cff094f88bfeac6d49551739ab35475abb334
andreypolyak <andrey.polyak@gmail.com>
2021-07-12 13:21:38 +0300

    Refactor freshener

--- appdaemon/apps/bathroom entrance/freshener.py
+++ appdaemon/apps/bathroom entrance/freshener.py
@@ -9,43 +9,53 @@ class Freshener(Base):
     self.handle = None
     self.listen_state(self.on_door_open, "binary_sensor.bathroom_door", new="on", old="off")
     self.listen_state(self.on_door_close, "binary_sensor.bathroom_door", new="off", old="on")
-    self.listen_state(self.on_arrive, "input_select.living_scene", old="away")
+    self.listen_state(self.on_not_away, "input_select.living_scene", old="away")
     for binary_sensor in self.get_state("binary_sensor"):
       if "bathroom" in binary_sensor and binary_sensor.endswith("_motion"):
         self.listen_state(self.on_motion, binary_sensor, new="on", old="off")
     self.listen_state(self.on_flush, "sensor.bathroom_toilet_vibration")
 
 
-  def on_arrive(self, entity, attribute, old, new, kwargs):
+  def on_not_away(self, entity, attribute, old, new, kwargs):
+    self.log("Scene was changed from away, spray")
     self.spray({})
 
 
   def on_flush(self, entity, attribute, old, new, kwargs):
-    if self.is_bad(new):
-      return
-    self.cancel_and_set_handle()
+    self.log("Toilet flush, schedule spray")
+    self.schedule_spray()
 
 
   def on_door_open(self, entity, attribute, old, new, kwargs):
-    self.cancel_and_set_handle()
+    self.log("Bathroom door open, schedule spray")
+    self.schedule_spray()
 
 
   def on_door_close(self, entity, attribute, old, new, kwargs):
+    self.log("Bathroom door closed, cancel spray")
     self.cancel_handle(self.handle)
 
 
   def on_motion(self, entity, attribute, old, new, kwargs):
-    self.cancel_and_set_handle()
+    if self.timer_running(self.handle):
+      self.log("Motion occured, schedule spray")
+      self.schedule_spray()
+
+
+  def schedule_spray(self):
+    self.cancel_handle(self.handle)
+    self.handle = self.run_in(self.spray, 15)
 
 
   def spray(self, kwargs):
     last_spray_ts = self.read_storage("last_spray_ts")
-    if self.get_delta_ts(last_spray_ts) > 600:
+    if self.get_delta_ts(last_spray_ts) < 600:
+      self.log("Spray cancelled because it occured recently")
+      return
+    if self.is_entity_on("binary_sensor.bathroom_door"):
+      self.log("Spray")
       self.turn_on_entity("switch.bathroom_freshener")
       self.write_storage("last_spray_ts", self.get_now_ts())
-
-
-  def cancel_and_set_handle(self):
-    if self.timer_running(self.handle):
-      self.cancel_timer(self.handle)
-      self.handle = self.run_in(self.spray, 15)
+    else:
+      self.log("Door is open, schedule spray")
+      self.schedule_spray()
