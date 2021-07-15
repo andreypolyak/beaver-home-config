9c92ad7f7eec7485edc15bfe685a33b3c40bac32
andreypolyak <andrey.polyak@gmail.com>
2021-07-15 17:18:08 +0300

    Do not turn off cooler if cover is closed

--- appdaemon/apps/kitchen/cooler.py
+++ appdaemon/apps/kitchen/cooler.py
@@ -7,11 +7,14 @@ class Cooler(Base):
     super().initialize()
     self.listen_state(self.on_change, "light.ha_group_kitchen", immediate=True)
     self.listen_state(self.on_change, "input_select.living_scene")
+    self.listen_state(self.on_change, "cover.kitchen_cover", attribute="current_position")
 
 
   def on_change(self, entity, attribute, old, new, kwargs):
+    is_cover_closed = self.get_state("cover.kitchen_cover", attribute="current_position") == 0
     is_lights_off = self.is_entity_off("light.ha_group_kitchen")
-    if self.get_living_scene() == "away" or (self.get_living_scene() == "night" and is_lights_off):
+    is_night_with_open_cover = self.get_living_scene() == "night" and is_lights_off and not is_cover_closed
+    if self.get_living_scene() == "away" or is_night_with_open_cover:
       self.turn_off_entity("switch.kitchen_cooler_plug")
     else:
       self.turn_on_entity("switch.kitchen_cooler_plug")
