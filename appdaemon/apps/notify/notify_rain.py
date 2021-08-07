3d17f78f095398bd9b97cdeba61e5d331e97465d
andreypolyak <andrey.polyak@gmail.com>
2021-08-08 00:10:10 +0300

    Create notify rain app

---
+++ appdaemon/apps/notify/notify_rain.py
@@ -0,0 +1,21 @@
+from base import Base
+
+
+class NotifyRain(Base):
+
+  def initialize(self):
+    super().initialize()
+    for sensor in self.get_state("sensor"):
+      if not sensor.endswith("_yandex_rain"):
+        continue
+      self.listen_state(self.on_change, sensor)
+
+
+  def on_change(self, entity, attribute, old, new, kwargs):
+    person_name = entity.replace("sensor.", "").replace("_yandex_rain", "")
+    if self.is_bad(new) or self.get_state(f"input_text.{person_name}_rain") == new:
+      return
+    self.set_value(f"input_text.{person_name}_rain", new)
+    if new == "В ближайшие 2 часа осадков не ожидается":
+      return
+    self.send_push(person_name, f"🌧️ {new}", "rain", min_delta=1800, url="/lovelace/outside")
