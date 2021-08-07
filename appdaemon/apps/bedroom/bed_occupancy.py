ddbac16437a04263bd2608da6117f19ffb7d4920
andreypolyak <andrey.polyak@gmail.com>
2021-08-08 00:07:37 +0300

    Fix bed occupancy

--- appdaemon/apps/bedroom/bed_occupancy.py
+++ appdaemon/apps/bedroom/bed_occupancy.py
@@ -8,7 +8,7 @@ class BedOccupancy(Base):
   def initialize(self):
     for sensor in SENSORS:
       self.listen_state(self.on_change, f"binary_sensor.{sensor}", new="off", old="on")
-    self.listen_state(self.on_change, "input_select.living_scene", new="day", old="night")
+    self.listen_state(self.on_change, "input_select.living_scene", old="night")
     self.listen_state(self.on_theo_bed_occupancy, "binary_sensor.bedroom_theo_bed_occupancy", new="on", old="off")
 
 
@@ -18,8 +18,6 @@ class BedOccupancy(Base):
     for sensor in SENSORS:
       if self.is_entity_on(f"binary_sensor.{sensor}"):
         return
-    if self.is_entity_off("binary_sensor.night_scene_enough"):
-      return
     self.log(f"Turning on day scene in sleeping zone because {entity} state changed")
     self.set_sleeping_scene("day")
 
