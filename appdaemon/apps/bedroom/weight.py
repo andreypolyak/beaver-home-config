5cdacf48e37539fbee9fa366f1f7ccef4364325e
andreypolyak <andrey.polyak@gmail.com>
2021-08-08 00:05:39 +0300

    Refactor weight

--- appdaemon/apps/bedroom/weight.py
+++ appdaemon/apps/bedroom/weight.py
@@ -5,30 +5,34 @@ class Weight(Base):
 
   def initialize(self):
     super().initialize()
+    self.last_changed_ts = 0
+    self.last_weight = 0
     default = {
       "current_weight": 0,
       "current_weight_ts": 0,
       "prev_weight": 0,
-      "prev_weight_ts": 0
+      "prev_weight_ts": 0,
+      "last_reported_ts": 0
     }
     for person_name in self.get_person_names():
       self.init_storage("weight", person_name, default)
-    self.listen_state(self.on_scales_change, "sensor.bedroom_scales")
+    self.listen_event(self.on_scales_change, "esphome.weight")
 
 
-  def on_scales_change(self, entity, attribute, old, new, kwargs):
-    if self.is_bad(new):
+  def on_scales_change(self, event_name, data, kwargs):
+    weight = round(float(data["weight"]), 2)
+    if weight < 10:
       return
-    weight = self.get_float_state(new)
-    if weight is None or weight < 10:
+    if self.last_weight == weight and self.get_delta_ts(self.last_changed_ts) > 1200:
       return
-    for person_name in self.get_person_names():
-      person_weight = self.get_float_state(f"input_number.{person_name}_weight")
-      if weight == person_weight:
-        return
+    self.last_changed_ts = self.get_now_ts()
+    self.last_weight = weight
     person = self.identify_person(weight)
     person_name = person["name"]
     person_phone = person["phone"]
+    person_state = self.read_storage(person_name, attribute="all")
+    if person_state["current_weight"] == weight and self.get_delta_ts(person_state["last_reported_ts"]) < 1200:
+      return
     self.set_value(f"input_number.{person_name}_weight", weight)
     (voice_message, message) = self.build_messages(person, weight)
     if person_phone:
@@ -55,58 +59,59 @@ class Weight(Base):
     person_name = person["name"]
     person_name_ru = person["ru_name"]
     person_state = self.read_storage(person_name, attribute="all")
-    if self.get_delta_ts(person_state["current_weight_ts"]) > 600:
+    if self.get_delta_ts(person_state["current_weight_ts"]) > 600 or person_state["prev_weight"] == 0:
       person_state = {
         "current_weight": weight,
         "current_weight_ts": self.get_now_ts(),
         "prev_weight": person_state["current_weight"],
-        "prev_weight_ts": person_state["current_weight_ts"]
+        "prev_weight_ts": person_state["current_weight_ts"],
+        "last_reported_ts": self.get_now_ts()
       }
     else:
       person_state = {
         "current_weight": weight,
         "current_weight_ts": self.get_now_ts(),
         "prev_weight": person_state["prev_weight"],
-        "prev_weight_ts": person_state["prev_weight_ts"]
+        "prev_weight_ts": person_state["prev_weight_ts"],
+        "last_reported_ts": self.get_now_ts()
       }
     self.write_storage(person_name, person_state, attribute="all")
     weight_change = (person_state["current_weight"] - person_state["prev_weight"]) * 1000
     weight_change = int(5 * round(weight_change / 5))
     weight_ts_change = person_state["current_weight_ts"] - person_state["prev_weight_ts"]
-    voice_message = f"{person_name_ru} весит {weight} килограмм. Вы "
-    message = f"⚖️ Weight: {weight} kg. ("
-    if weight_change < 0:
-      voice_message += f"похудели на {abs(weight_change)} грамм "
-      message += f"-{abs(weight_change)} g. "
-    elif weight_change > 0:
-      voice_message += f"поправились на {abs(weight_change)} грамм "
-      message += f"+{abs(weight_change)} g. "
-    else:
-      voice_message += "не изменились в весе "
-      message += "+0 g. "
-    voice_message += "с момента последнего взвешивания "
-    message += "vs. "
-    if weight_ts_change > 86400:
-      days = round(weight_ts_change / 86400)
-      days_text_ru = self.morph_periods(days, "day", "ru")
-      days_text_en = self.morph_periods(days, "day", "en")
-      voice_message += f"{days} {days_text_ru} назад"
-      message += f"{days} {days_text_en} ago"
-    elif weight_ts_change > 3600:
-      hours = round(weight_ts_change / 3600)
-      hours_text_ru = self.morph_periods(hours, "hour", "ru")
-      hours_text_en = self.morph_periods(hours, "hour", "en")
-      voice_message += f"{hours} {hours_text_ru} назад"
-      message += f"{hours} {hours_text_en} ago"
-    else:
-      minutes = round(weight_ts_change / 60)
-      minutes_text_ru = self.morph_periods(minutes, "minute", "ru")
-      minutes_text_en = self.morph_periods(minutes, "minute", "en")
-      voice_message += f"{minutes} {minutes_text_ru} назад"
-      message += f"{minutes} {minutes_text_en} ago"
+    voice_message = f"{person_name_ru} весит {weight} килограмм"
+    message = f"⚖️ Weight: {weight} kg"
+    if person_state["prev_weight"] != 0:
+      voice_message += ". Вы "
+      message += ". ("
+      if weight_change < 0:
+        voice_message += f"похудели на {abs(weight_change)} грамм "
+        message += f"-{abs(weight_change)} g. "
+      elif weight_change > 0:
+        voice_message += f"поправились на {abs(weight_change)} грамм "
+        message += f"+{abs(weight_change)} g. "
+      else:
+        voice_message += "не изменились в весе "
+        message += "+0 g. "
+      voice_message += "с момента последнего взвешивания "
+      message += "vs. "
+      if weight_ts_change > 86400:
+        periods = round(weight_ts_change / 86400)
+        text_ru = self.morph_periods(periods, "day", "ru")
+        text_en = self.morph_periods(periods, "day", "en")
+      elif weight_ts_change > 3600:
+        periods = round(weight_ts_change / 3600)
+        text_ru = self.morph_periods(periods, "hour", "ru")
+        text_en = self.morph_periods(periods, "hour", "en")
+      else:
+        periods = round(weight_ts_change / 60)
+        text_ru = self.morph_periods(periods, "minute", "ru")
+        text_en = self.morph_periods(periods, "minute", "en")
+      voice_message += f"{periods} {text_ru} назад"
+      message += f"{periods} {text_en} ago)"
     if person["phone"]:
       voice_message += ". Не забудьте нажать на уведомление, чтобы сохранить вес в Apple Health"
-      message += "). Click to save in Apple Health"
+      message += ". Click to save in Apple Health"
     return (voice_message, message)
 
 
