5bb65096660bc83ac88142281e3f5e61d419bfc5
andreypolyak <andrey.polyak@gmail.com>
2021-08-12 16:57:25 +0300

    Refactor living room cover

--- appdaemon/apps/living room/living_room_cover.py
+++ appdaemon/apps/living room/living_room_cover.py
@@ -5,50 +5,53 @@ class LivingRoomCover(Base):
 
   def initialize(self):
     super().initialize()
-    self.prev_position = None
-    self.was_closed = False
+    self.check_handle = None
+    self.turn_off_handle = None
+    self.init_storage("living_room_cover", "was_closed", False)
     self.listen_state(self.on_scene_change, "input_select.living_scene")
     self.listen_state(self.on_balcony_door_open, "binary_sensor.living_room_balcony_door", new="on", old="off")
     self.listen_state(self.on_balcony_door_close, "binary_sensor.living_room_balcony_door", new="off", old="on")
-    self.listen_state(self.on_cover_running_change, "cover.living_room_cover", attribute="running", immediate=True)
-    self.listen_state(self.on_cover_switch_change, "sensor.living_room_cover_switch")
+    entity = "cover.living_room_cover"
+    self.listen_state(self.on_cover_running_change, entity, attribute="running", immediate=True)
+    self.listen_state(self.on_cover_position_change, entity, attribute="current_position", immediate=True)
     self.listen_event(self.on_close_living_room_cover, "close_living_room_cover")
-    self.run_every(self.get_cover_state, "now", 60)
+    self.run_every(self.get_cover_state, "now", 30)
 
 
   def get_cover_state(self, kwargs):
     self.call_service("mqtt/publish", topic="zigbee2mqtt/Living Room Cover/get", payload='{"state": ""}')
 
 
-  def on_cover_switch_change(self, entity, attribute, old, new, kwargs):
-    if new not in ["open", "close"]:
-      return
-    if new == "close" and self.is_cover_closed():
-      return
-    if new == "open" and not self.is_cover_closed():
-      return
-    self.log(f"Cover got command from the switch: {new}. Setting it active")
-    self.turn_on_entity("input_boolean.living_room_cover_active")
-    self.run_in(self.update_position, 8)
-
-
-  def update_position(self, kwargs):
-    position = self.get_state("cover.living_room_cover", attribute="current_position")
-    if position != self.prev_position or position not in [0, 100]:
-      self.log(f"Cover changed position ({position}). Getting fresh update")
-      self.get_cover_state({})
-      self.turn_on_entity("input_boolean.living_room_cover_active")
-      self.prev_position = position
-      self.run_in(self.update_position, 4)
-    else:
-      self.log("Cover didn't change position. Setting it inactive")
-      self.turn_off_entity("input_boolean.living_room_cover_active")
-
-
   def on_cover_running_change(self, entity, attribute, old, new, kwargs):
     if new is True:
-      self.turn_on_entity("input_boolean.living_room_cover_active")
-      self.run_in(self.update_position, 8)
+      self.turn_on_active()
+      return
+    self.turn_off_active()
+
+
+  def on_cover_position_change(self, entity, attribute, old, new, kwargs):
+    if new not in [0, 100]:
+      self.turn_on_active()
+      return
+    self.turn_off_active()
+
+
+  def turn_on_active(self):
+    self.turn_on_entity("input_boolean.living_room_cover_active")
+    self.cancel_handle(self.check_handle)
+    self.cancel_handle(self.turn_off_handle)
+    self.check_handle = self.run_every(self.get_cover_state, "now", 1)
+
+
+  def turn_off_active(self):
+    self.turn_off_entity("input_boolean.living_room_cover_active")
+    self.cancel_handle(self.turn_off_handle)
+    self.turn_off_handle = self.run_in(self.turn_off_check, 15)
+
+
+  def turn_off_check(self, kwargs):
+    self.cancel_handle(self.check_handle)
+    self.cancel_handle(self.turn_off_handle)
 
 
   def on_close_living_room_cover(self, event_name, data, kwargs):
@@ -57,38 +60,31 @@ class LivingRoomCover(Base):
 
   def on_scene_change(self, entity, attribute, old, new, kwargs):
     if new in ["away", "night", "light_cinema", "dark_cinema", "party"]:
+      self.write_storage("was_closed", True)
       self.close_living_room_cover()
     elif new == "day":
-      self.was_closed = False
-      self.open_living_room_cover()
-    elif old == "away":
-      self.was_closed = False
+      self.write_storage("was_closed", False)
       self.open_living_room_cover()
 
 
   def on_balcony_door_open(self, entity, attribute, old, new, kwargs):
     if self.is_cover_closed():
-      self.log("Balcony door is open but cover is closed. Saving that cover was closed")
-      self.was_closed = True
-      self.open_living_room_cover()
+      self.write_storage("was_closed", True)
     else:
-      self.log("Balcony door is open and cover is open. Saving that cover was not closed")
-      self.was_closed = False
+      self.write_storage("was_closed", False)
+    self.open_living_room_cover()
 
 
   def on_balcony_door_close(self, entity, attribute, old, new, kwargs):
     if self.get_living_scene() in ["away", "night", "light_cinema", "dark_cinema", "party"]:
       self.log("Balcony door closed. Closing cover because of the current living scene")
       self.close_living_room_cover()
-    elif self.was_closed:
-      self.log("Balcony door closed. Closing cover beacuse it was closed before the door was open")
+    elif self.read_storage("was_closed"):
+      self.log("Balcony door closed. Closing cover because it was closed before the door was open")
       self.close_living_room_cover()
 
 
   def close_living_room_cover(self):
-    if self.is_cover_closed():
-      self.log("Cover will not close because it's aready closed")
-      return
     if self.is_entity_off("binary_sensor.living_room_balcony_door"):
       self.log("Close cover")
       self.close_cover("living_room_cover")
@@ -97,16 +93,16 @@ class LivingRoomCover(Base):
 
 
   def open_living_room_cover(self):
-    if self.is_cover_closed():
-      self.log("Open cover")
-      self.open_cover("living_room_cover")
-    else:
-      self.log("Cover will not open because it's aready open")
+    self.log("Open cover")
+    self.open_cover("living_room_cover")
 
 
   def is_cover_closed(self):
     if self.is_entity_on("input_boolean.living_room_cover_active"):
+      self.log("Cover not closed because it's active")
       return False
     if self.get_state("cover.living_room_cover") == "open":
+      self.log("Cover not closed because it's open")
       return False
+    self.log("Cover is not closed")
     return True
