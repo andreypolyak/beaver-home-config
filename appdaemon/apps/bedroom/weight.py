a17801f1756bb584c1dac4e4ac510d7338a9f03d
andreypolyak <andrey.polyak@gmail.com>
2021-11-01 17:38:19 +0300

    Refactor weight

--- appdaemon/apps/bedroom/weight.py
+++ appdaemon/apps/bedroom/weight.py
@@ -23,7 +23,7 @@ class Weight(Base):
 
 
   def on_scales_change(self, event_name, data, kwargs):
-    last = person_state = self.read_storage("last", attribute="all")
+    last = self.read_storage("last", attribute="all")
     weight = round(float(data["weight"]), 2)
     if weight < 10:
       return
@@ -36,33 +36,20 @@ class Weight(Base):
     self.write_storage("last", last, attribute="all")
     person = self.identify_person(weight)
     person_name = person["name"]
-    person_phone = person["phone"]
-    person_state = self.read_storage(person_name, attribute="all")
-    if person_state["current_weight"] == weight and self.get_delta_ts(person_state["last_reported_ts"]) < 1200:
+    old_person_state = self.read_storage(person_name, attribute="all")
+    if old_person_state["current_weight"] == weight and self.get_delta_ts(old_person_state["last_reported_ts"]) < 1200:
       return
     self.set_value(f"input_number.{person_name}_weight", weight)
-    (voice_message, message) = self.build_messages(person, weight)
-    if person_phone:
+    new_person_state = self.build_person_state(person_name, weight)
+    self.write_storage(person_name, new_person_state, attribute="all")
+    (voice_message, message) = self.build_messages(person, new_person_state)
+    if person["phone"]:
       self.send_push(person_name, message, "weight", sound="Calypso.caf", url=self.build_url(weight))
     if self.sleeping_scene != "night":
       self.fire_event("yandex_speak_text", text=voice_message, room="bedroom")
 
 
-  def identify_person(self, weight):
-    weight_delta = 9999
-    selected_person = None
-    for person in self.get_persons():
-      person_name = person["name"]
-      person_weight = self.get_float_state(f"input_number.{person_name}_weight")
-      if abs(person_weight - weight) < weight_delta:
-        weight_delta = abs(person_weight - weight)
-        selected_person = person
-    return selected_person
-
-
-  def build_messages(self, person, weight):
-    person_name = person["name"]
-    person_name_ru = person["ru_name"]
+  def build_person_state(self, person_name, weight):
     person_state = self.read_storage(person_name, attribute="all")
     if self.get_delta_ts(person_state["current_weight_ts"]) > 600 or person_state["prev_weight"] == 0:
       person_state = {
@@ -80,13 +67,31 @@ class Weight(Base):
         "prev_weight_ts": person_state["prev_weight_ts"],
         "last_reported_ts": self.get_now_ts()
       }
-    self.write_storage(person_name, person_state, attribute="all")
+    return person_state
+
+
+  def identify_person(self, weight):
+    weight_delta = 9999
+    selected_person = None
+    for person in self.get_persons():
+      person_name = person["name"]
+      person_weight = self.get_float_state(f"input_number.{person_name}_weight")
+      if abs(person_weight - weight) < weight_delta:
+        weight_delta = abs(person_weight - weight)
+        selected_person = person
+    return selected_person
+
+
+  def build_messages(self, person, person_state):
+    weight = person_state["current_weight"]
+    prev_weight = person_state["prev_weight"]
     weight_change = (person_state["current_weight"] - person_state["prev_weight"]) * 1000
     weight_change = int(5 * round(weight_change / 5))
     weight_ts_change = person_state["current_weight_ts"] - person_state["prev_weight_ts"]
-    voice_message = f"{person_name_ru} весит {weight} килограмм"
+    person_ru_name = person["ru_name"]
+    voice_message = f"{person_ru_name} весит {weight} килограмм"
     message = f"⚖️ Weight: {weight} kg"
-    if person_state["prev_weight"] != 0:
+    if prev_weight:
       voice_message += ". Вы "
       message += ". ("
       if weight_change < 0:
