c34d7bf727ee487c62d2f3b8debe8da31b65eb7e
andreypolyak <andrey.polyak@gmail.com>
2021-11-10 20:04:28 +0300

    Use occupation sensor in freshener

--- appdaemon/apps/bathroom entrance/freshener.py
+++ appdaemon/apps/bathroom entrance/freshener.py
@@ -6,18 +6,16 @@ class Freshener(Base):
   def initialize(self):
     super().initialize()
     self.init_storage("freshener", "last_spray_ts", 0)
-    self.toilet_motion = False
+    self.toilet_was_occupied = False
     self.listen_state(self.on_light_off, "light.ha_group_bathroom_entrance", new="off", old="on")
     self.listen_state(self.on_not_away, "input_select.living_scene", old="away")
-    for binary_sensor in self.get_state("binary_sensor"):
-      if "bathroom" in binary_sensor and binary_sensor.endswith("_motion"):
-        self.listen_state(self.on_motion, binary_sensor, new="on", old="off")
+    self.listen_state(self.on_occupation, "binary_sensor.bathroom_toilet_occupancy", new="off", old="on")
 
 
   def on_light_off(self, entity, attribute, old, new, kwargs):
-    if self.toilet_motion:
+    if self.toilet_was_occupied:
       self.spray()
-    self.toilet_motion = False
+    self.toilet_was_occupied = False
 
 
   def on_not_away(self, entity, attribute, old, new, kwargs):
@@ -25,9 +23,9 @@ class Freshener(Base):
     self.spray()
 
 
-  def on_motion(self, entity, attribute, old, new, kwargs):
-    self.log("Motion occured, schedule spray")
-    self.toilet_motion = True
+  def on_occupation(self, entity, attribute, old, new, kwargs):
+    self.log("Toilet was occupied, schedule spray")
+    self.toilet_was_occupied = True
 
 
   def spray(self):
