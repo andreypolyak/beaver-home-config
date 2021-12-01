49fc6541bda1538083a1993e5dcab012ee7991a7
andreypolyak <andrey.polyak@gmail.com>
2021-12-01 21:24:58 +0300

    Circadian updates

--- appdaemon/apps/global/circadian_update.py
+++ appdaemon/apps/global/circadian_update.py
@@ -1,11 +1,11 @@
 from base import Base
 import math
 
-DELAY = 180
+DELAY = 90
 SAT_STEP = 2
 MAX_LUX = 200
-MIN_LUX = 0
-# From 06:00 to 12:00 decrease saturation by maximum 20% (100% max saturation at 6, 80% at 9, 100% at 12)
+MIN_LUX = 6
+# Between 06:00-12:00 decrease saturation by maximum 20% (100% max saturation at 6, 80% at 9, 100% at 12)
 MORNING_START = 6
 MORNING_END = 12
 PCT = 0.2
@@ -42,16 +42,9 @@ class CircadianUpdate(Base):
     if lux is None:
       self.log("Can't read balcony illuminance value")
       return None
-    last_seen = self.get_int_state("sensor.balcony_illuminance", attribute="last_seen")
-    if last_seen is None:
-      self.log("Can't get last seen parameter for balcony illuminance sensor")
-      return None
-    if self.get_delta_ts(last_seen) > 7200:
-      self.log("Balcony illuminance sensor last seen more than 2 hours ago")
-      return None
     if lux >= MAX_LUX:
       return 0
-    elif lux < MIN_LUX:
+    elif lux <= MIN_LUX:
       return 100
     else:
       saturation = int(math.ceil((MAX_LUX - lux) / ((MAX_LUX - MIN_LUX) / (100 / SAT_STEP - 1)))) * SAT_STEP
