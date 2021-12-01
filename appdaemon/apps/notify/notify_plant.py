923ad3f30450049fdef9f92f2030548b37f1bc2f
andreypolyak <andrey.polyak@gmail.com>
2021-12-01 21:26:06 +0300

    Add moisture threshold and new moisture sensors

--- appdaemon/apps/notify/notify_plant.py
+++ appdaemon/apps/notify/notify_plant.py
@@ -17,7 +17,9 @@ class NotifyPlant(Base):
     for zone in ["living", "sleeping"]:
       if self.get_scene(zone) == "night":
         return
-    if moisture < 80:
+    plant_name = entity.replace("sensor.", "")
+    threshold = self.get_int_state(f"input_number.{plant_name}_threshold")
+    if moisture < threshold:
       plant_name = entity.replace("sensor.", "").replace("_moisture", "").replace("_", " ").title()
       message = f"🪴 It's time to water the {plant_name}. Moisture there is {moisture}%"
       self.send_push("home_or_none", message, "plant", min_delta=21600)
